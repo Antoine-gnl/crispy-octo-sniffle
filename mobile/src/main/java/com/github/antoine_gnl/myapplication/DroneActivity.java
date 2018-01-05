39f0de0af02cf13b670a58d89aaea43e215bc3b4
antoine <antoine.genouel@free.fr>
2018-01-05 16:16:33 +0100

    unknown updates

--- mobile/src/main/java/com/github/antoine_gnl/myapplication/DroneActivity.java
+++ mobile/src/main/java/com/github/antoine_gnl/myapplication/DroneActivity.java
@@ -1,8 +1,10 @@
 package com.github.antoine_gnl.myapplication;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -10,13 +12,13 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.antoine_gnl.myapplication.SensorTagAdapter.SensorTagAdapterOnClickHandler;
 import com.github.antoine_gnl.myapplication.WatchAdapter.WatchAdapterOnClickHandler;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +36,8 @@ public class DroneActivity extends AppCompatActivity implements SensorTagAdapter
     private BluetoothAdapter mBluetoothAdapter;
     private boolean mScanning;
     private Handler mHandler;
+
+    private static final int REQUEST_ENABLE_BT = 1;
 
     void createFakeData() {
         for (int i = 100; i >0; i--)
@@ -90,6 +94,7 @@ public class DroneActivity extends AppCompatActivity implements SensorTagAdapter
         // Use this check to determine whether BLE is supported on the device.  Then you can
         // selectively disable BLE-related features.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Log.i(BluetoothLeService.class.getSimpleName(),"J'ai pas de ble");
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
         }
@@ -98,16 +103,28 @@ public class DroneActivity extends AppCompatActivity implements SensorTagAdapter
         // BluetoothAdapter through BluetoothManager.
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        Log.i(BluetoothLeService.class.getSimpleName(),"bluetooth initialized");
         mBluetoothAdapter = bluetoothManager.getAdapter();
 
         // Checks if Bluetooth is supported on the device.
         if (mBluetoothAdapter == null) {
+            Log.i(BluetoothLeService.class.getSimpleName(),"J'ai pas de dents bleues");
             Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
             finish();
             return;
         }
 
         createFakeData();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
